4bcc71ca390bea9b62ca18ce367d80e14d1e678e
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-06-08 20:16:10 +0300

    тест на создание заказа

--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -6,7 +6,6 @@ describe('Тест конструктора бургера', () => {
     beforeEach(() => {
         setCookie('accessToken', 'kekusToken');
         localStorage.setItem('refreshToken', 'kekusRefreshToken');
-        cy.log(URL);
         cy.intercept('GET', `${URL}/auth/user`, { fixture: 'user.json' }).as('getUser');
         cy.intercept('GET', `${URL}/ingredients`, {fixture: 'ingredients.json'}).as('getIngredients');
         cy.visit('http://localhost:4000/');
@@ -21,23 +20,44 @@ describe('Тест конструктора бургера', () => {
 
     it('Получение списка ингредиентов в конструкторе', () => {
         cy.get('[data-cy="constructor"]').as('constructor');
-        cy.get('[data-cy="ingredient"]').eq(3).find('button').click();
+        cy.get('[data-cy="ingredient"]').eq(10).find('button').click();
         cy.get('[data-cy="ingredient"]').eq(1).find('button').click();
 
 
-        cy.get('@constructor').should('contain', 'Мясо бессмертных моллюсков Protostomia');
+        cy.get('@constructor').should('contain', 'Что тут делает булка?');
         cy.get('@constructor').should('contain', 'Флюоресцентная булка R2-D3');
     });
 
     it('Проверка модельного окна ингридиента', () => {
-        cy.get('[data-cy="ingredient"]').eq(3).click();
+        cy.get('[data-cy="ingredient"]').eq(10).click();
         cy.get('[data-cy="modal"]').as('modal');
 
         cy.get('@modal').should('exist');
-        cy.get('@modal').should('contain', 'Мясо бессмертных моллюсков Protostomia');
+        cy.get('@modal').should('contain', 'Что тут делает булка?');
+
+        cy.get('[data-cy="close-modal"]').click();
+        cy.get('@modal').should('not.exist');
+    })
+
+    it('Создание заказа', () => {
+        cy.intercept('POST', `${URL}/orders`, {fixture: 'order.json'}).as('createOrder');
+        cy.get('[data-cy="constructor"]').as('constructor');
+
+        cy.get('[data-cy="ingredient"]').eq(10).find('button').click();
+        cy.get('[data-cy="ingredient"]').eq(1).find('button').click();
+
+        cy.get('@constructor').find('button').contains('Оформить заказ').click();
+        cy.get('[data-cy="modal"]').as('modal');
+
+        cy.get('@modal').should('exist');
+        cy.get('@modal').should('contain', '133712');
 
         cy.get('[data-cy="close-modal"]').click();
         cy.get('@modal').should('not.exist');
 
+        cy.get('@constructor').should('not.contain', 'Что тут делает булка?');
+        cy.get('@constructor').should('not.contain', 'Флюоресцентная булка R2-D3');
+
+        cy.wait('@createOrder');
     })
 })
