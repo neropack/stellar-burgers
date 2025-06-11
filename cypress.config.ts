85193da8e061a3e13c983acd104fe90acc0e9f4e
Dmitriy Meshcheryakov <WindlessMist@yandex.ru>
2025-06-11 22:57:46 +0300

    исправления cypress тестов

--- cypress.config.ts
+++ cypress.config.ts
@@ -5,6 +5,7 @@ export default defineConfig({
     BURGER_API_URL: 'https://norma.nomoreparties.space/api'
   },
   e2e: {
+    baseUrl: 'http://localhost:4000/',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     }
--- cypress/e2e/constructor.cy.ts
+++ cypress/e2e/constructor.cy.ts
@@ -1,16 +1,17 @@
 import { setCookie, deleteCookie } from '../../src/utils/cookie';
 
-const URL = Cypress.env('BURGER_API_URL');
-
 describe('Тест конструктора бургера', () => {
+
     beforeEach(() => {
         setCookie('accessToken', 'kekusToken');
         localStorage.setItem('refreshToken', 'kekusRefreshToken');
-        cy.intercept('GET', `${URL}/auth/user`, { fixture: 'user.json' }).as('getUser');
-        cy.intercept('GET', `${URL}/ingredients`, {fixture: 'ingredients.json'}).as('getIngredients');
-        cy.visit('http://localhost:4000/');
+        cy.intercept('GET', 'user', { fixture: 'user.json' }).as('getUser');
+        cy.intercept('GET', 'ingredients', {fixture: 'ingredients.json'}).as('getIngredients');
+        cy.visit('/');
         cy.wait('@getUser');
         cy.wait('@getIngredients');
+        cy.get('[data-cy="constructor"]').as('constructor');
+        cy.get('[data-cy="ingredient"]').as('ingredients');
     });
 
     afterEach(() => {
@@ -19,17 +20,15 @@ describe('Тест конструктора бургера', () => {
     });
 
     it('Получение списка ингредиентов в конструкторе', () => {
-        cy.get('[data-cy="constructor"]').as('constructor');
-        cy.get('[data-cy="ingredient"]').eq(10).find('button').click();
-        cy.get('[data-cy="ingredient"]').eq(1).find('button').click();
-
+        cy.get('@ingredients').eq(10).find('button').click();
+        cy.get('@ingredients').eq(1).find('button').click();
 
         cy.get('@constructor').should('contain', 'Что тут делает булка?');
         cy.get('@constructor').should('contain', 'Флюоресцентная булка R2-D3');
     });
 
     it('Проверка модельного окна ингридиента', () => {
-        cy.get('[data-cy="ingredient"]').eq(10).click();
+        cy.get('@ingredients').eq(10).click();
         cy.get('[data-cy="modal"]').as('modal');
 
         cy.get('@modal').should('exist');
@@ -40,11 +39,10 @@ describe('Тест конструктора бургера', () => {
     })
 
     it('Создание заказа', () => {
-        cy.intercept('POST', `${URL}/orders`, {fixture: 'order.json'}).as('createOrder');
-        cy.get('[data-cy="constructor"]').as('constructor');
+        cy.intercept('POST', 'orders', {fixture: 'order.json'}).as('createOrder');
 
-        cy.get('[data-cy="ingredient"]').eq(10).find('button').click();
-        cy.get('[data-cy="ingredient"]').eq(1).find('button').click();
+        cy.get('@ingredients').eq(10).find('button').click();
+        cy.get('@ingredients').eq(1).find('button').click();
 
         cy.get('@constructor').find('button').contains('Оформить заказ').click();
         cy.get('[data-cy="modal"]').as('modal');
